86786c5fce0dda6463e1f7c2f5adf98333900f39
hitsuji24 <2xx4xx.mole.melajis@gmail.com>
2023-12-18 01:16:53 +0900

    検証用にダミーデータを用意

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ document.getElementById('nutrientsForm').addEventListener('submit', function (ev
     const maxCalories = document.getElementById('maxCalories').value;
 
     //* 1日分の数値を朝昼晩用に分割
-    //? リクエストが1ptで結果表示が0.1ptなので費用を安くするためにもっと適した設計ができそう
+    //? リクエストが1ptで結果表示が0.1ptなので費用を安くするためにもっと適した設計ができそう→朝晩をLight,昼をHeavyにするとか。結果を使いまわしするとか。
+    //? テスト段階でいっぱいAPI使ってしまうと無料プランの上限にきてしまう。何かいい策はないか？ CSSのためならダミーでレスポンスを飛ばすとか？
     // 分割のための関数
     const calculatePortions = (value) => {
         return {
@@ -40,32 +41,276 @@ document.getElementById('nutrientsForm').addEventListener('submit', function (ev
     console.log(apiUrlLunch);
     console.log(apiUrlDinner);
 
+
     //*結果を1週間分の献立として表示 
-    // APIレスポンスを処理し、レシピを表示する関数
-    function fetchAndDisplayMeals(apiUrl, mealType) {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(recipes => {
-                // データを日ごとに分配して表示
-                recipes.forEach((recipe, index) => {
-                    const day = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'][index];
-                    const recipeElement = document.createElement('div');
-                    recipeElement.innerHTML = `
-                    <h3>${day.toUpperCase()}: ${recipe.title}</h3>
-                    <img src="${recipe.image}" alt="${recipe.title}">
-                    <p>Calories: ${recipe.calories}</p>
-                    <p>Carbs: ${recipe.carbs}</p>
-                    <p>Fat: ${recipe.fat}</p>
-                    <p>Protein: ${recipe.protein}</p>
-                `;
-                    document.querySelector(`.${day}Plan`).appendChild(recipeElement);
-                });
-            })
-            .catch(error => console.error('Error:', error));
+    // // APIレスポンスを処理し、レシピを表示する関数
+    // function fetchAndDisplayMeals(apiUrl, mealType) {
+    //     fetch(apiUrl)
+    //         .then(response => response.json())
+    //         .then(recipes => {
+    //             // データを日ごとに分配して表示
+    //             recipes.forEach((recipe, index) => {
+    //                 const day = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'][index];
+    //                 const mealTime = ['Morning', 'Lunch', 'Dinner'][index % 3];
+
+    //                 const recipeElement = document.createElement('div');
+    //                 recipeElement.innerHTML = `
+    //                 <h3>${day.toUpperCase()}: ${recipe.title}</h3>
+    //                 <img src="${recipe.image}" alt="${recipe.title}">
+    //                 <p>Calories: ${recipe.calories}</p>
+    //                 <p>Carbs: ${recipe.carbs}</p>
+    //                 <p>Fat: ${recipe.fat}</p>
+    //                 <p>Protein: ${recipe.protein}</p>
+    //             `;
+    //                 document.querySelector(`.${day}${mealTime}`).appendChild(recipeElement);
+    //             });
+    //         })
+    //         .catch(error => console.error('Error:', error));
+    // }
+    // // 朝食、昼食、夕食用のAPIリクエストを実行
+    // fetchAndDisplayMeals(apiUrlBreakfast, 'breakfast');
+    // fetchAndDisplayMeals(apiUrlLunch, 'lunch');
+    // fetchAndDisplayMeals(apiUrlDinner, 'dinner');
+
+    //* ダミーを使って検証するとき用 
+    // 検証用のダミーデータ
+    const dummyRecipesBreakfast = [
+        {
+            "id": 632810,
+            "title": "Asian Chicken",
+            "image": "https://spoonacular.com/recipeImages/632810-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 320,
+            "protein": "26g",
+            "fat": "7g",
+            "carbs": "37g"
+        },
+        {
+            "id": 633376,
+            "title": "Bagels",
+            "image": "https://spoonacular.com/recipeImages/633376-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 295,
+            "protein": "29g",
+            "fat": "4g",
+            "carbs": "36g"
+        },
+        {
+            "id": 633508,
+            "title": "Baked Cheese Manicotti",
+            "image": "https://spoonacular.com/recipeImages/633508-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 441,
+            "protein": "28g",
+            "fat": "12g",
+            "carbs": "46g"
+        },
+        {
+            "id": 634165,
+            "title": "Banana Prawn Rolls",
+            "image": "https://spoonacular.com/recipeImages/634165-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 342,
+            "protein": "24g",
+            "fat": "5g",
+            "carbs": "51g"
+        },
+        {
+            "id": 638199,
+            "title": "Chicken Mulligatawny Soup",
+            "image": "https://spoonacular.com/recipeImages/638199-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 368,
+            "protein": "27g",
+            "fat": "10g",
+            "carbs": "38g"
+        },
+        {
+            "id": 652250,
+            "title": "Molasses and Cayenne Pork Loin",
+            "image": "https://spoonacular.com/recipeImages/652250-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 332,
+            "protein": "27g",
+            "fat": "5g",
+            "carbs": "44g"
+        },
+        {
+            "id": 663151,
+            "title": "Thai Shrimp",
+            "image": "https://spoonacular.com/recipeImages/663151-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 355,
+            "protein": "27g",
+            "fat": "7g",
+            "carbs": "42g"
+        }
+    ];
+    const dummyRecipesLunch = [
+        {
+            "id": 632812,
+            "title": "Asian Chicken and Broccoli With Chili Garlic Sauce",
+            "image": "https://spoonacular.com/recipeImages/632812-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 546,
+            "protein": "33g",
+            "fat": "9g",
+            "carbs": "77g"
+        },
+        {
+            "id": 636360,
+            "title": "Brussels Sprout Carbonara with Fettuccini",
+            "image": "https://spoonacular.com/recipeImages/636360-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 549,
+            "protein": "38g",
+            "fat": "17g",
+            "carbs": "56g"
+        },
+        {
+            "id": 637897,
+            "title": "Chicken and Chickpea Chili",
+            "image": "https://spoonacular.com/recipeImages/637897-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 610,
+            "protein": "37g",
+            "fat": "18g",
+            "carbs": "64g"
+        },
+        {
+            "id": 642594,
+            "title": "Farfalle with Shrimps, Tomatoes Basil Sauce",
+            "image": "https://spoonacular.com/recipeImages/642594-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 570,
+            "protein": "38g",
+            "fat": "8g",
+            "carbs": "81g"
+        },
+        {
+            "id": 642929,
+            "title": "Fish Congee",
+            "image": "https://spoonacular.com/recipeImages/642929-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 441,
+            "protein": "36g",
+            "fat": "5g",
+            "carbs": "57g"
+        },
+        {
+            "id": 659638,
+            "title": "Seafood Gumbo",
+            "image": "https://spoonacular.com/recipeImages/659638-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 376,
+            "protein": "35g",
+            "fat": "5g",
+            "carbs": "43g"
+        },
+        {
+            "id": 982365,
+            "title": "Instant Pot Hawaiian Chicken",
+            "image": "https://spoonacular.com/recipeImages/982365-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 473,
+            "protein": "38g",
+            "fat": "5g",
+            "carbs": "65g"
+        }
+    ];
+    const dummyRecipesDinner = [
+        {
+            "id": 632810,
+            "title": "Asian Chicken",
+            "image": "https://spoonacular.com/recipeImages/632810-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 320,
+            "protein": "26g",
+            "fat": "7g",
+            "carbs": "37g"
+        },
+        {
+            "id": 633376,
+            "title": "Bagels",
+            "image": "https://spoonacular.com/recipeImages/633376-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 295,
+            "protein": "29g",
+            "fat": "4g",
+            "carbs": "36g"
+        },
+        {
+            "id": 633508,
+            "title": "Baked Cheese Manicotti",
+            "image": "https://spoonacular.com/recipeImages/633508-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 441,
+            "protein": "28g",
+            "fat": "12g",
+            "carbs": "46g"
+        },
+        {
+            "id": 634165,
+            "title": "Banana Prawn Rolls",
+            "image": "https://spoonacular.com/recipeImages/634165-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 342,
+            "protein": "24g",
+            "fat": "5g",
+            "carbs": "51g"
+        },
+        {
+            "id": 638199,
+            "title": "Chicken Mulligatawny Soup",
+            "image": "https://spoonacular.com/recipeImages/638199-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 368,
+            "protein": "27g",
+            "fat": "10g",
+            "carbs": "38g"
+        },
+        {
+            "id": 652250,
+            "title": "Molasses and Cayenne Pork Loin",
+            "image": "https://spoonacular.com/recipeImages/652250-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 332,
+            "protein": "27g",
+            "fat": "5g",
+            "carbs": "44g"
+        },
+        {
+            "id": 663151,
+            "title": "Thai Shrimp",
+            "image": "https://spoonacular.com/recipeImages/663151-312x231.jpg",
+            "imageType": "jpg",
+            "calories": 355,
+            "protein": "27g",
+            "fat": "7g",
+            "carbs": "42g"
+        }
+    ]
+    // ダミーデータを使って表示
+    function displayDummyMeals(dummyRecipes, mealType) {
+        dummyRecipes.forEach((recipe, index) => {
+            const day = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'][index];
+            const mealTime = ['Morning', 'Lunch', 'Dinner'][index % 3];
+
+            const recipeElement = document.createElement('div');
+            recipeElement.innerHTML = `
+                <h3>${day.toUpperCase()}: ${recipe.title}</h3>
+                <img src="${recipe.image}" alt="${recipe.title}">
+                <p>Calories: ${recipe.calories}</p>
+                <p>Carbs: ${recipe.carbs}</p>
+                <p>Fat: ${recipe.fat}</p>
+                <p>Protein: ${recipe.protein}</p>
+            `;
+            document.querySelector(`.${day}${mealTime}`).appendChild(recipeElement);
+        });
     }
-    // 朝食、昼食、夕食用のAPIリクエストを実行
-    fetchAndDisplayMeals(apiUrlBreakfast, 'breakfast');
-    fetchAndDisplayMeals(apiUrlLunch, 'lunch');
-    fetchAndDisplayMeals(apiUrlDinner, 'dinner');
+    // 表示
+    displayDummyMeals(dummyRecipesBreakfast, 'breakfast');
+    displayDummyMeals(dummyRecipesLunch, 'lunch');
+    displayDummyMeals(dummyRecipesDinner, 'dinner');
 
 });
