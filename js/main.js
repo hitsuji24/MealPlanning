fcf26884cd1e7956fb12a23399ca7d3a6483ef6a
hitsuji24 <2xx4xx.mole.melajis@gmail.com>
2023-12-18 00:27:02 +0900

    1週間分のレシピを出力できた

--- js/main.js
+++ js/main.js
@@ -31,15 +31,41 @@ document.getElementById('nutrientsForm').addEventListener('submit', function (ev
     const minCaloriesPortions = calculatePortions(minCalories);
     const maxCaloriesPortions = calculatePortions(maxCalories);
 
-
     //* APIリクエストの作成
-    const apiUrlBreakfast = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=APIkeyHERE&minProtein=${minProteinPortions.breakfast}&maxProtein=${maxProteinPortions.breakfast}&minFat=${minFatPortions.breakfast}&maxFat=${maxFatPortions.breakfast}&minCarbs=${minCarbsPortions.breakfast}&maxCarbs=${maxCarbsPortions.breakfast}&minCalories=${minCaloriesPortions.breakfast}&maxCalories=${maxCaloriesPortions.breakfast}`;
-    const apiUrlLunch = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=APIkeyHERE&minProtein=${minProteinPortions.lunch}&maxProtein=${maxProteinPortions.lunch}&minFat=${minFatPortions.lunch}&maxFat=${maxFatPortions.lunch}&minCarbs=${minCarbsPortions.lunch}&maxCarbs=${maxCarbsPortions.lunch}&minCalories=${minCaloriesPortions.lunch}&maxCalories=${maxCaloriesPortions.lunch}`;
-    const apiUrlDinner = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=APIkeyHERE&minProtein=${minProteinPortions.dinner}&maxProtein=${maxProteinPortions.dinner}&minFat=${minFatPortions.dinner}&maxFat=${maxFatPortions.dinner}&minCarbs=${minCarbsPortions.dinner}&maxCarbs=${maxCarbsPortions.dinner}&minCalories=${minCaloriesPortions.dinner}&maxCalories=${maxCaloriesPortions.dinner}`;
+    const apiUrlBreakfast = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=XXX&minProtein=${minProteinPortions.breakfast}&maxProtein=${maxProteinPortions.breakfast}&minFat=${minFatPortions.breakfast}&maxFat=${maxFatPortions.breakfast}&minCarbs=${minCarbsPortions.breakfast}&maxCarbs=${maxCarbsPortions.breakfast}&minCalories=${minCaloriesPortions.breakfast}&maxCalories=${maxCaloriesPortions.breakfast}&number=7`;
+    const apiUrlLunch = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=XXX&minProtein=${minProteinPortions.lunch}&maxProtein=${maxProteinPortions.lunch}&minFat=${minFatPortions.lunch}&maxFat=${maxFatPortions.lunch}&minCarbs=${minCarbsPortions.lunch}&maxCarbs=${maxCarbsPortions.lunch}&minCalories=${minCaloriesPortions.lunch}&maxCalories=${maxCaloriesPortions.lunch}&number=7`;
+    const apiUrlDinner = `https://api.spoonacular.com/recipes/findByNutrients?apiKey=XXX&minProtein=${minProteinPortions.dinner}&maxProtein=${maxProteinPortions.dinner}&minFat=${minFatPortions.dinner}&maxFat=${maxFatPortions.dinner}&minCarbs=${minCarbsPortions.dinner}&maxCarbs=${maxCarbsPortions.dinner}&minCalories=${minCaloriesPortions.dinner}&maxCalories=${maxCaloriesPortions.dinner}&number=7`;
     // URLの確認
     console.log(apiUrlBreakfast);
     console.log(apiUrlLunch);
     console.log(apiUrlDinner);
 
+    //*結果を1週間分の献立として表示 
+    // APIレスポンスを処理し、レシピを表示する関数
+    function fetchAndDisplayMeals(apiUrl, mealType) {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(recipes => {
+                // データを日ごとに分配して表示
+                recipes.forEach((recipe, index) => {
+                    const day = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'][index];
+                    const recipeElement = document.createElement('div');
+                    recipeElement.innerHTML = `
+                    <h3>${day.toUpperCase()}: ${recipe.title}</h3>
+                    <img src="${recipe.image}" alt="${recipe.title}">
+                    <p>Calories: ${recipe.calories}</p>
+                    <p>Carbs: ${recipe.carbs}</p>
+                    <p>Fat: ${recipe.fat}</p>
+                    <p>Protein: ${recipe.protein}</p>
+                `;
+                    document.querySelector(`.${day}Plan`).appendChild(recipeElement);
+                });
+            })
+            .catch(error => console.error('Error:', error));
+    }
+    // 朝食、昼食、夕食用のAPIリクエストを実行
+    fetchAndDisplayMeals(apiUrlBreakfast, 'breakfast');
+    fetchAndDisplayMeals(apiUrlLunch, 'lunch');
+    fetchAndDisplayMeals(apiUrlDinner, 'dinner');
 
 });
